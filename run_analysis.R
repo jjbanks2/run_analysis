4da62cba47b1a54c7519f3e2b7ff8bf5213aa9b4
jjbanks2 <jenbanks2@gmail.com>
2023-11-06 12:04:58 -0800

    Refactor mean extraction.  Add creation of new tidy dataset with averages.  Var name clean up

--- run_analysis.R
+++ run_analysis.R
@@ -1,82 +1,81 @@
 
-
-
-
-
-#read files
-
-#2 cols, V1 and V2?, Activity ID, Activity Name
+# Read files
 # activity_labels<-read.table("activity_labels.txt", header=FALSE)
-# 
-# # 2 cols, feature ID? and feature name, id matches up with x cols
 # features<-read.table("features.txt", header=FALSE)
-# 
-# # 561 cols, 2947 rows. of measurement data.  Cols V1 etc
 # X_test<- read.table("./test/X_test.txt",header=FALSE)
-# 
-# # 1 col, 2947 rows Activity ID
 # y_test<- read.table("./test/y_test.txt",header=FALSE)
-# 
-# # 1 col, 2947 rows 1-24, Subject ID?
 # subject_test<- read.table("./test/subject_test.txt",header=FALSE)
-# 
-# # 561 cols, 7352 rows, measurement data
 # X_train<- read.table("./train/X_train.txt",header=FALSE)
-# 
-# # 1 col, Activity ID
 # y_train<- read.table("./train/y_train.txt",header=FALSE)
-# 
-# # 1 col, Subject ID?
 # subject_train<-read.table("./train/subject_train.txt",header=FALSE)
 
-# Appropriately labels the data set with descriptive variable names.
+# Appropriately label the dataset with descriptive variable names
 colnames(activity_labels) <- c("Activity ID", "Activity")
 colnames(features) <-c("Feature ID", "Feature")
-colnames(X_test) <- c(features$Feature) 
+colnames(X_test) <- c(features$Feature)
 colnames(y_test) <-c("Activity")
 colnames(subject_test) <-c("Subject ID")
-colnames(X_train) <- c(features$Feature) 
+colnames(X_train) <- c(features$Feature)
 colnames(y_train) <- c("Activity")
-colnames(subject_train) <- c("Subject ID") #convert to numeric?
+colnames(subject_train) <- c("Subject ID") 
 
 
-# 1.  Merge the training and the test sets to create one data set.
-
-# combine test and train data into one column (merge rows)
+# Combine test and train data into one column (merge rows)
 xtesttrain <- rbind(X_test, X_train)
 ytesttrain <-rbind(y_test, y_train)
 stesttrain <-rbind(subject_test, subject_train)
 
-# combine features, activity and subject columns for each measurement set
+# Combine features, activity and subject columns for each measurement set
 d<-cbind(stesttrain, ytesttrain, xtesttrain, stringsAsFactors=FALSE)
 
 # Replace activity Id's with activity names
 for (x in 1:length(d$Activity)) {
     if (d$Activity[x] == 1) {
-        d$Activity[x] <- "Walking"
+        d$Activity[x] <- "walking"
     }
     if (d$Activity[x] == 2) {
-        d$Activity[x] <- "Walking Upstairs"
+        d$Activity[x] <- "walking upstairs"
     }
     if (d$Activity[x] == 3) {
-        d$Activity[x] <- "Walking Downstairs"
+        d$Activity[x] <- "walking downstairs"
     }
     if (d$Activity[x] == 4) {
-        d$Activity[x] <- "Sitting"
+        d$Activity[x] <- "sitting"
     }
     if (d$Activity[x] == 5) {
-        d$Activity[x] <- "Standing"
+        d$Activity[x] <- "standing"
     }
     if (d$Activity[x] == 6) {
-        d$Activity[x] <- "Laying"
+        d$Activity[x] <- "laying"
     }
 }
 
-# Extracts only the measurements on the mean and standard deviation
-# for each measurement, adds to new dataframe
-msddata<-grep("mean\\(\\)|std\\(\\)", features$Feature)
+# Extract measurements for mean & standard deviation for each measurement
+msddata<-c(as.character(features$Feature[grep("mean\\(\\)|std\\(\\)", features$Feature)]))
 msd<-d[, msddata]
 
-# From the data set in step 4, creates a second, independent
-# tidy data set with the average of each variable for each 
+# From the new dataset, create a second, independent
+# tidy data set with the average of each variable for each
 # activity and each subject.
+newset<-cbind(msd, d$`Subject ID`, d$Activity)
+tidy<-aggregate(newset, by=list(newset$`d$\`Subject ID\``, newset$`d$Activity`),
+                FUN=mean)
+
+# Rename new aggregate columns
+colnames(tidy)[1]<-"Subject"
+colnames(tidy)[2]<-"Activity"
+
+# Delete legacy subject and activity columns
+tidy<-tidy[,-69:-70]
+
+# Variable name clean up
+names(tidy)<-gsub("\\()", "", names(tidy))
+names(tidy)<-gsub("^t", "time", names(tidy))
+names(tidy)<-gsub("^f", "freq", names(tidy))
+
+#Output file
+write.table(tidy, "./tidy.txt", row.name=FALSE)
+
+
+
+ 
